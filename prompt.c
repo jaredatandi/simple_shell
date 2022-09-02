1a8c9a1475ea5bebaca6d9c5ffd15a689856e241
jaredatandi <jaredatandi07@gmail.com>
2022-09-02 22:42:43 +0300

    improve parsing line

--- prompt.c
+++ prompt.c
@@ -20,11 +20,18 @@
 
 int main(int argc, char **argv)
 {
-        char **p_line;
+        char **p_line, **argp;
+        int i = 1;
+
+        p_line = parse_line(argv[1]);
         if (argc >= 2)
         {
-                p_line = parse_line(argv[1]);
-                execvp(p_line[0], p_line);
+                while (argv[i] != NULL)
+                {
+                        argp = parse_line(argv[i]);
+                        execvp(p_line[0], argp);
+                        i++;
+                }
         } else
                 _getline();
         return (0);
